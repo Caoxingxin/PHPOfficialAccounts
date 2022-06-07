5def76090d00bb8aaf2e245dd6ea44e055d68f8b
cxx-7920 <1265105488@qq.com>
2022-06-07 15:09:41 +0800

    队列测试

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Factory\TempFactory;
+use App\Helps\Functions;
+use App\Http\Model\WeChatUser;
+use App\Jobs\TestJob;
+use Illuminate\Support\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -17,5 +22,20 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+    public function testweather(){
+        $weatherData = [
+            //高德地图控制台的key
+            'key' => 'd5c63b169d3ffbedfc579d27dde9107e',
+            'city' => '440106',
+            'extensions' => 'all',
+            'output' => 'JSON'
+        ];
+        //$result = json_decode((new Functions())->curlGet('https://restapi.amap.com/v3/weather/weatherInfo',$weatherData),true);
+        //dd($result);
+        $date = Carbon::now();
+        //dd((new TempFactory())->setTemp('yxl'));
+        TestJob::dispatch();
+        dd(123);
     }
 }
